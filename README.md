2331c8729d2086eb9f713b6b70bb41fbb0c67ab5
Jesus Zen Droïd <jczd@engrenage.ch>
2023-04-16 10:08:58 +0200

    clock is working :-)

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # pchess
-cHEXs is yet another chess variant to be played on a hexagonal lattice 
+pchess is yet another [chess variant](https://en.wikipedia.org/wiki/Hexagonal_chess) to be played on a hexagonal lattice.
+
+While not being fundamentally different than some other variants, it was specifically designed for games involving three (or more) players while trying to keep as close as possible to the original chess rules.
+
+## The novelty
+
+When involving more then two players, pchess **must** be played with a clock (standard chess clocks are not suited) ; it is always the player who has the most time left on their clock whose turn it is to play, unless that player just finished a turn. The initial playing order is determined by the clock in a pseudo-random fashion.
+
+## Board setup
+
+There are a variety of possible board setups, one of which is shown below and features three knights.
+
+![Board setup](game_setups/pchess_board-setup-3p.png)
+
+By reducing the number of pieces used, it is possible to accomodate for up to 6 players for massive free-for-all warfare.
+
+The `manual/` directory contains more info of piece movement and board setups.
+
+
+### Example
+
+Alice, Bob and Charles are playing a game. Each player starts with 5 minutes (300 seconds) of time available.
+
+* Bob starts, executes his move in 5 seconds.
+* Alice comes next, and moves in 15 seconds.
+* It's now Charles' turn ; however, he's feeling confident (and mabye somewhat distracted) so he choses to go make himself a cup of tea and come backs at the table 3 minutes later, having only 120 seconds left on his clock.
+* Since bob has now 10 more seconds available than Alice, he gets to play.
+* Then it's Alice's turn, because she definitely still has more time on her clock than Charles
+* After comes Bob's turn (he also definitely has more time than Charles), then Alice, and Bob, and Alice and so on until either one of them drops his time below the 120 seconds mark, where Charles will finally be able to play.
+
+If the above description of events may seem a little silly, it is because it is. There are, however, cases where a player who is satisfied with his position on the board and whishes to stay there a little while might willingly chose to play somewhat slower than usual in order to force another player's turn. Or may start to panick when they realize they are playing too slow and being more observers than contestants.
+
+
+## Software implementation
+
+Since clocks are fairly difficult to make, and fancy clocks with multiple mechanisms, random number generators and all the little gizmos inside are even more difficult to build, a simple python program was written to achieve a simiéar result.
+
+At the moment, it uses [pyGame](https://www.pygame.org) but that may change in the future
+
+![Screenshot](clock.png)
+
+
+### Features
+
+* supports any number of players (including 2)
+* player names can be specified
+
+
+### TODO
+
+* packaging for Android, IOs (maybe use Kivy?)
+* *handicap* a feature : player's time runs faster by a preset amount
+* player colors
+* *Pause* does not really work, barely good enough to wait for all players to be ready to start the game
---
+++ clock.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+
+FRAMEBUFFER=False	# WIP
+
+import pygame
+from sys import argv, exit
+from random import choice
+from time import sleep
+from datetime import datetime
+
+bgcolor = (10, 10, 10)
+xgcolor = (255, 0, 192)
+fgcolor = (159, 182, 205)
+xpcolor = (255, 0, 0)
+green = (0, 255, 0)
+fontsize = 42
+linespace = -0
+dotlines = 12
+
+try:
+	timeout = argv[2]*1000
+except IndexError:
+	timeout = 5*60*1000
+try:
+	num_players = argv[1]
+except IndexError:
+	num_players = 3
+
+class NextPlayer(Exception): pass
+class GameOver(Exception): pass
+
+players = {}
+for p in range(num_players):
+	n = input(f"Player {p} name: ")
+	players[n if n else f"Player {p}"] = timeout
+
+def display(players, playing, t, nextp):
+	pos_x = screen.get_rect().centerx/2
+	pos_y = screen.get_rect().centery
+
+	i = 0
+	for p in players.keys():
+		if p == playing:
+			text = font.render(
+					f"{p}: {t/1000:.3f} [s]",
+					True, fgcolor, bgcolor
+				)
+		else:
+			if p == nextp:
+				text = font.render(
+						">",
+						True, xgcolor, bgcolor
+					)
+				screen.blit(text, (pos_x-fontsize/2, pos_y+((i)*(fontsize+linespace))) )
+			text = font.render(
+					f"{p}: {players[p]/1000:.3f} [s]",
+					True, xgcolor, bgcolor
+				)
+		screen.blit(text, (pos_x, pos_y+((i)*(fontsize+linespace))) )
+		i += 1
+
+
+
+pygame.init()
+if FRAMEBUFFER:
+	os.putenv('SDL_FBDEV', '/dev/fb0')
+	os.environ["SDL_VIDEODRIVER"] = 'fbcon'
+	xmax = pygame.display.Info().current_w
+	ymax = pygame.display.Info().current_h
+	print("framebuffer size: %d x %d" % (self.xmax, self.ymax))
+	screen = pygame.display.set_mode((xmax, ymax), pygame.FULLSCREEN)
+else:
+	screen = pygame.display.set_mode( (800,600) )
+pygame.display.init()
+
+#print(pygame.display.list_modes(  ))
+pygame.display.set_caption('pchess clock')
+screen.fill(bgcolor)
+
+font = pygame.font.Font(None, fontsize)
+
+
+try:
+	running = False
+	#now = int(perf_counter()*1000)
+	now = int(datetime.now().timestamp()*1000)
+	candidate = choice(list(players.keys()))
+	nextp = None
+
+	while True:
+		try:
+			r = (players[candidate]-(int(datetime.now().timestamp()*1000)-now))
+			if r <= 0:
+				print(f"{candidate} has run out of time!",end='\r')
+				del(players[candidate])
+				screen.fill(bgcolor)
+				if len(players) == 1:
+					raise GameOver
+				else:
+					raise NextPlayer
+			#print(f"{candidate}: {r/1000:0.3f} [s]", end='\r')
+
+
+			display( players, candidate, r, nextp )
+			if not running:
+				# TODO pausing doesn't really work... messes up the timing
+				now = int(datetime.now().timestamp()*1000)
+
+				text = font.render(
+						"PAUSED",
+						True, xpcolor, bgcolor
+					)
+				textRect = text.get_rect()
+				textRect.centerx = screen.get_rect().centerx
+				textRect.centery = screen.get_rect().centery
+				screen.blit(text, textRect)
+				#pygame.display.update()
+			pygame.display.update()
+
+			pygame.event.pump()
+			keys = pygame.key.get_pressed()
+
+			if keys[pygame.K_q]:
+				exit()
+			elif keys[pygame.K_ESCAPE]:
+				running = not running
+				sleep(.3)
+				screen.fill(bgcolor)
+			elif keys[pygame.K_RETURN] or keys[pygame.K_SPACE]:
+				screen.fill(bgcolor)
+				if not running:
+					running = True
+				else:
+					raise NextPlayer
+		except NextPlayer:
+			P = players.copy()
+			try:
+				del P[candidate]
+				players[candidate] = r
+			except KeyError:
+				pass
+			now = int(datetime.now().timestamp()*1000)
+			mx = max(P.values())
+			candidates = [key for key, value in P.items() if value == mx]
+			
+			if len(candidates) != 1:
+				candidate = choice(candidates)
+			else:
+				candidate = candidates[0]
+
+			P = players.copy()
+			del P[candidate]
+			nextp = max(P, key=P.get)
+
+			#print(f"{candidate}: {players[candidate]/1000:0.3f} [s]", end='\r')
+			sleep(.3)
+		except GameOver:
+			print(f"\nGame over! the winner is {list(players.keys())[0]}")
+			break
+
+
+except KeyboardInterrupt:
+	print(f"\nGame finished!")
+
+
